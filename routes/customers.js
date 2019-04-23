b188b9cb9f21a21d5d4fc0661a4f8f910d73f4c1
Abdoul Diallo <committed1@gmail.com>
2019-04-23 01:40:13 -0400

    Get customers

--- routes/customers.js
+++ routes/customers.js
@@ -1,8 +1,14 @@
 const errors = require('restify-errors');
+const customer = require('../models/Customer');
 
 module.exports = server => {
-	server.get('/customers', (req, res, next) => {
-		res.send({ msg: 'test' });
-		next();
+	server.get('/customers', async (req, res, next) => {
+		try {
+			const customers = await Customer.find({});
+			res.send(customers);
+			next();
+		} catch (err) {
+			return next(new errors.InvalidContentError(err));
+		}
 	});
 };
