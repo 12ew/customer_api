818db37556ea0e3d612745309a23f900043044a5
Abdoul Diallo <committed1@gmail.com>
2019-04-23 00:31:17 -0400

    Add customer model

---
+++ models/Customer.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const timestamp = require('mongoose-timestamp');
+const Schema = mongoose.Schema;
+
+const CustomerSchema = new Schema({
+	name: {
+		type: String,
+		required: true,
+		trim: true
+	},
+	email: {
+		type: String,
+		required: true,
+		trim: true
+	},
+	phone_number: {
+		type: Number,
+		required: true,
+		trim: true
+	}
+});
+
+CustomerSchema.plugin(timestamp);
+const Customer = mongoose.model('Customer', CustomerSchema);
+module.exports = Customer;
