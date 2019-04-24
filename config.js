c35dabd22cb92fe45e9f3aa188c9334e12180837
Abdoul Diallo <committed1@gmail.com>
2019-04-24 00:23:22 -0400

    Add auth and jwt

--- config.js
+++ config.js
@@ -4,5 +4,6 @@ module.exports = {
 	URL: process.env.BASE_URL || 'http://localhost:3000',
 	MONGODB_URI:
 		process.env.MONGODB_URI ||
-		'mongodb+srv://abc123:0JNJAbMBSkfE91uw@cluster0-q4e4h.mongodb.net/test?retryWrites=true'
+		'mongodb+srv://abc123:0JNJAbMBSkfE91uw@cluster0-q4e4h.mongodb.net/test?retryWrites=true',
+	JWT_SECRET: process.env.JWT_SECRET || 'soso123'
 };
--- routes/customers.js
+++ routes/customers.js
@@ -1,5 +1,7 @@
 const errors = require('restify-errors');
+const rjwt = require('restify-jwt-community');
 const Customer = require('../models/Customer');
+const config = require('../config');
 
 module.exports = server => {
 	//GET Customers
@@ -29,7 +31,7 @@ module.exports = server => {
 	});
 
 	// POST / Add Customer
-	server.post('/customers', async (req, res, next) => {
+	server.post('/customers', rjwt({ secret: config.JWT_SECRET }), async (req, res, next) => {
 		//Check for JSON
 		if (!req.is('application/json')) {
 			return next(new error.InvalidContentError("Expects 'application/json'"));
@@ -53,7 +55,7 @@ module.exports = server => {
 	});
 
 	// Update Customer
-	server.put('/customers/:id', async (req, res, next) => {
+	server.put('/customers/:id', rjwt({ secret: config.JWT_SECRET }), async (req, res, next) => {
 		//Check for JSON
 		if (!req.is('application/json')) {
 			return next(new error.InvalidContentError("Expects 'application/json'"));
@@ -76,7 +78,7 @@ module.exports = server => {
 	});
 
 	// Delete Customer
-	server.del('/customers/:id', async (req, res, next) => {
+	server.del('/customers/:id', rjwt({ secret: config.JWT_SECRET }), async (req, res, next) => {
 		try {
 			const customer = await Customer.findOneAndRemove({ _id: req.params.id });
 			res.send(204);
